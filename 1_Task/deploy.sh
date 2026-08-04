3da66b4a51184c36f0bb6b6554710ea6c76abcce
0TheMagik <izzan.nsyarif@gmail.com>
2026-08-04 19:02:38 +0700

    update: deploy.sh change fromlinked file to normal copy

--- 1_Task/deploy.sh
+++ 1_Task/deploy.sh
@@ -6,7 +6,7 @@ CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
 sudo apt update
 sudo apt install -y nginx
 
-sudo ln -sfn "$CURRENT_DIR/index.html" /var/www/html/index.html
+sudo cp -f "$CURRENT_DIR/index.html" /var/www/html/index.html
 
 sudo ufw allow 'Nginx HTTP'
 sudo ufw enable
